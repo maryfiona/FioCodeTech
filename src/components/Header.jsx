980d1b55793e07ef0688722a8e06f0b612f43545
maryfiona <aguchidimmamaryfiona@gmail.com>
2024-09-10 20:03:42 +0100

    done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -37,7 +37,7 @@ const Header = () => {
                         <h2 className="typed-text-output d-inline"></h2>
                        
                         <div className="d-flex align-items-center pt-5">
-                        <a href="/public/FioCodeTech (2).pdf" className="btn btn-primary py-3 px-4 me-5" download="FioCodeTech.pdf">Download CV</a>
+                        <a href="/FioCodeTech (2).pdf" className="btn btn-primary py-3 px-4 me-5" download="FioCodeTech.pdf">Download CV</a>
 
                             
                            
