36aa82fa88472ebc27a911ebfb32207e7a928129
maryfiona <aguchidimmamaryfiona@gmail.com>
2024-12-18 20:24:08 +0100

    link to project added

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -43,6 +43,104 @@ const Projects = () => {
           </div>
          
         </div>
+        <div>
+  <h3 style={{ textAlign: 'center', marginBottom: '20px' }}>Links to Projects</h3>
+  <div>
+    <a 
+      href="https://uche-com.onrender.com/" 
+      target="_blank" 
+      rel="noopener noreferrer" 
+      style={{ 
+        display: 'block', 
+        color: '#007bff', 
+        textDecoration: 'none', 
+        fontSize: '16px', 
+        fontWeight: 'bold', 
+        marginBottom: '10px' 
+      }}
+    >
+      Uche Com
+    </a>
+    <a 
+      href="https://fiocalculator.onrender.com/" 
+      target="_blank" 
+      rel="noopener noreferrer" 
+      style={{ 
+        display: 'block', 
+        color: '#007bff', 
+        textDecoration: 'none', 
+        fontSize: '16px', 
+        fontWeight: 'bold', 
+        marginBottom: '10px' 
+      }}
+    >
+      Fio Calculator
+    </a>
+    <a 
+      href="https://fiocodetech.onrender.com/" 
+      target="_blank" 
+      rel="noopener noreferrer" 
+      style={{ 
+        display: 'block', 
+        color: '#007bff', 
+        textDecoration: 'none', 
+        fontSize: '16px', 
+        fontWeight: 'bold', 
+        marginBottom: '10px' 
+      }}
+    >
+      Fio Code Tech
+    </a>
+    <a 
+      href="https://real-estate-dtvw.onrender.com/" 
+      target="_blank" 
+      rel="noopener noreferrer" 
+      style={{ 
+        display: 'block', 
+        color: '#007bff', 
+        textDecoration: 'none', 
+        fontSize: '16px', 
+        fontWeight: 'bold', 
+        marginBottom: '10px' 
+      }}
+    >
+      Real Estate
+    </a>
+    <a 
+      href="https://agu-chidimma-maryfiona-eazipay.netlify.app/" 
+      target="_blank" 
+      rel="noopener noreferrer" 
+      style={{ 
+        display: 'block', 
+        color: '#007bff', 
+        textDecoration: 'none', 
+        fontSize: '16px', 
+        fontWeight: 'bold', 
+        marginBottom: '10px' 
+      }}
+    >
+      EaziPay
+    </a>
+    <a 
+      href="https://blog-io-io59.onrender.com/" 
+      target="_blank" 
+      rel="noopener noreferrer" 
+      style={{ 
+        display: 'block', 
+        color: '#007bff', 
+        textDecoration: 'none', 
+        fontSize: '16px', 
+        fontWeight: 'bold', 
+        marginBottom: '10px' 
+      }}
+    >
+      Blog IO
+    </a>
+  </div>
+</div>
+
+
+
       </div>
     </div>
   );
